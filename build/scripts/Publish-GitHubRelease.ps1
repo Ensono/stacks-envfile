8a877a09838593c15fc77a5f985dde86f8066cea
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2022-04-28 13:03:42 +0100

    Debug github request

--- build/scripts/Publish-GitHubRelease.ps1
+++ build/scripts/Publish-GitHubRelease.ps1
@@ -154,6 +154,8 @@ $releaseArgs = @{
     ErrorAction = "Stop"
 }
 
+$releaseArgs
+
 # Create the release by making the API call, artifacts will be uploaded afterwards
 Write-Output ("Creating release for: {0}" -f $version)
 try {
